c9315f482729234089ba5adb76150a7a41105095
Alan Li <alandron06281990@gmail.com>
2019-12-20 19:36:32 -0800

    add logo and heading

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
 import classes from './Header.module.css';
+import logo from '../../assets/img/logo.png';
 
 const header = () => {
     return (
         <header className={classes.Header}>
-            some text...
+            <div className={classes.LogoContainer}>
+                <img src={logo} alt="logo" className={classes.Logo} />
+            </div>
+
+            <div className={classes.TextBox}>
+                <h1 className={classes.HeadingPrimary}>
+                    <span className={classes.HeadingPrimaryMain}>Hi, I'm Tianyu Li</span>
+                    <span className={classes.HeadingPrimarySub}>I'm a full-stack developer and a UX designer</span>
+                </h1>
+            </div>
+
+            
         </header>
     )
 }
