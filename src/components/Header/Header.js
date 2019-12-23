0d85a4613707547188a8ca9aad2c60ed28530d55
Alan Li <alandron06281990@gmail.com>
2019-12-22 19:20:18 -0800

    done

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import classes from './Header.module.css';
+import classes from './Header.module.scss';
 import logo from '../../assets/img/logo.png';
 import Button from '../UI/Button/Button';
 
