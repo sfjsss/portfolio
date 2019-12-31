f39464f49066d64735e8525251c3851f22361aa1
Alan Li <alandron06281990@gmail.com>
2019-12-30 22:21:42 -0800

    create process component

---
+++ src/components/Process/Process.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import classes from './Process.module.scss';
+
+const process = () => {
+    return (
+        <section className={classes.process}>
+
+        </section>
+    )
+}
+
+export default process;
