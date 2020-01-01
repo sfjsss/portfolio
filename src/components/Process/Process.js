6f6f47a3639bb5f08fcac640dc1aba6ff9c3d5e8
Alan Li <alandron06281990@gmail.com>
2019-12-31 23:59:37 -0800

    done

--- src/components/Process/Process.js
+++ src/components/Process/Process.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 import classes from './Process.module.scss';
+import design_icon from '../../assets/img/process_icons/design.svg';
 import research_icon from '../../assets/img/process_icons/research.svg';
+import develop_icon from '../../assets/img/process_icons/develop.svg';
+import testing_icon from '../../assets/img/process_icons/testing.svg';
 
 const process = () => {
     return (
@@ -13,6 +16,30 @@ const process = () => {
                         <img src={research_icon} className={classes['process-box__icon']}/>
                         <h3 className={[classes['heading-tertiary'], classes['u-margin-bottom-small']].join(' ')}>Research</h3>
                         <p className={classes['process-box__text']}>For each of my projects, I always conduct a research before the actual coding. It helps me better understanding my client and their goal.</p>
+                    </div>
+                </div>
+
+                <div className={classes['col-1-of-4']}>
+                    <div className={classes['process-box']}>
+                        <img src={design_icon} className={classes['process-box__icon']}/>
+                        <h3 className={[classes['heading-tertiary'], classes['u-margin-bottom-small']].join(' ')}>Design</h3>
+                        <p className={classes['process-box__text']}>Based on my research, I would translate my design into a prototype. I then communicate with the client through the prototype and collect feedback.</p>
+                    </div>
+                </div>
+
+                <div className={classes['col-1-of-4']}>
+                    <div className={classes['process-box']}>
+                        <img src={develop_icon} className={classes['process-box__icon']}/>
+                        <h3 className={[classes['heading-tertiary'], classes['u-margin-bottom-small']].join(' ')}>Develop</h3>
+                        <p className={classes['process-box__text']}>Using my improved prototype as a guideline, I would conduct a system design regarding what frameworks and tools to use and then code the project.</p>
+                    </div>
+                </div>
+
+                <div className={classes['col-1-of-4']}>
+                    <div className={classes['process-box']}>
+                        <img src={testing_icon} className={classes['process-box__icon']}/>
+                        <h3 className={[classes['heading-tertiary'], classes['u-margin-bottom-small']].join(' ')}>Testing</h3>
+                        <p className={classes['process-box__text']}>After the first build of the project, I usually invite the client for a beta test. I will then improve the project based on the test result and feedback.</p>
                     </div>
                 </div>
             </div>
