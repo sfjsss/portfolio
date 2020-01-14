bc49652c5bfde8047701ae6e90ae74a0ec581f6c
Alan Li <alandron06281990@gmail.com>
2020-01-13 23:04:46 -0800

    add animation on navigation button

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,10 +3,16 @@ import React from 'react';
 import classes from './Navigation.module.scss';
 
 const navigation = () => {
+    const uncheckbox = () => {
+        document.getElementById('navi-toggle').checked = false;
+    }
+
     return (
         <div className={classes['navigation']}>
             <input type='checkbox' className={classes['checkbox']} id='navi-toggle' />
-            <label htmlFor='navi-toggle' className={classes['button']}>MENU</label>
+            <label htmlFor='navi-toggle' className={classes['button']}>
+                <span className={classes['icon']}>&nbsp;</span>
+            </label>
             <div className={classes['background']}>&nbsp;</div>
             <nav className={classes['nav']}>
                 <ul className={classes['list']}>
@@ -17,7 +23,7 @@ const navigation = () => {
                         <a href='#' className={classes['link']}>Process</a>
                     </li>
                     <li className={classes['item']}>
-                        <a href='#myWorks' className={classes['link']}>Works</a>
+                        <a href='#myWorks' className={classes['link']} onClick={uncheckbox}>Works</a>
                     </li>
                     <li className={classes['item']}>
                         <a href='#' className={classes['link']}>Contact</a>
